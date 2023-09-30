df5d04604a3abab9cfea9111eef85ef40ab32377
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2023-09-30 19:42:33 -0300

    Update index.php

--- relatorio_internet/index.php
+++ relatorio_internet/index.php
@@ -11,13 +11,13 @@
     $_dbhost = "localhost";
     $_dbuser = "root";
     $_dbpass = "";
-    $minha_conexao = new mysqli($_dbhost, $_dbuser, $_dbpass);
-    if ($minha_conexao -> connect_errno) {
-        print("Falha na Conexão com o banco de dados: %s</br>". $minha_conexao->connect_errno);
+    $_minha_conexao = new mysqli($_dbhost, $_dbuser, $_dbpass);
+    if ($_minha_conexao -> connect_errno) {
+        print("Falha na Conexão com o banco de dados: %s</br>". $_minha_conexao->connect_errno);
         exit();
     }
     printf("Conexão bem sucedida!");
-    
+    $_retval = mysqli_select_db($_minha_conexao, )
     ?>
 <body>
     
