06210fffa70386a0aef59913dee07c5956534709
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2023-09-30 14:07:03 -0300

    Create index.php

---
+++ relatorio_internet/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Informações da INTERNET</title>
+</head>
+<body>
+    <h1>status da internet</h1>
+</body>
+</html>
