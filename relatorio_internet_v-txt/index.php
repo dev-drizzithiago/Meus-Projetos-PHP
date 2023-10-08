f32a1c5db605d43c6db39bfba0e66d0da2b6d152
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2023-10-08 11:04:53 -0300

    Create index.php

---
+++ relatorio_internet_v-txt/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Relatorio Internet_v-txt</title>
+</head>
+<body>
+    <?php 
+        $_local_registro_on = "G:/Meu Drive/Estudos/Python/Arquivos de texto/Relatorio_internet/registro_ping_local_on.log"
+    
+    ?>    
+</body>
+</html>
