71397d6173b0a91c92a0b55ad50f18c6b796d7be
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2023-10-08 17:20:45 -0300

    Update index.php

--- relatorio_internet_v-txt/index.php
+++ relatorio_internet_v-txt/index.php
@@ -10,14 +10,7 @@
         $_local_registro_on = "C:/wamp64/www/Meus Projetos/Meus-Projetos-PHP/relatorio_internet_v-txt/registro_ping_local_on.txt";
         $_internet_ping_on = "C:/wamp64/www/Meus Projetos/Meus-Projetos-PHP/relatorio_internet_v-txt/registro_ping_internet_on.txt";
 
-        $_leitura_local_on = fopen("$_local_registro_on", "r");
-        $_leitura_internet_on = fopen("$_internet_ping_on", "r");
-        while(!feof($_leitura_local_on)){
-            echo fgetc($_leitura_local_on);
-        }
-        while(!feof($_internet_ping_on)) {
-            echo fgetc($_internet_ping_on);
-        }
+        
     ?>    
 </body>
 </html>
