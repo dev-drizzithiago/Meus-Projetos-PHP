693b671404bfbe5d16dc6189410a4576f0f3754f
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2023-10-17 21:10:32 -0300

    Update index.php

--- relatorio_internet_v-txt_v2/index.php
+++ relatorio_internet_v-txt_v2/index.php
@@ -8,16 +8,21 @@
 </head>
 
 <?php 
-    
-
-
+    $_local_arq_LAN = "_status_LAN.log";
+    $_local_arq_WAN = "_status_WAN.log";
+    $_valor_status_array_LAN = file_get_contents ($_local_arq_LAN);
+    $_valor_status_array_WAN = file_get_contents($_local_arq_WAN);
+    $_status_LAN = explode("-", $_valor_status_array_LAN);
+    $_status_WAN = explode("-", $_valor_status_array_WAN);
 ?>
 
 <body>
     <main>
+        <div>
         <?php 
-        
+            echo "$_status_LAN";
         ?>
+        </div>
     </main>
 </body>
 </html>
