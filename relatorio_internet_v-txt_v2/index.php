71c060954b3bc6b5e2dc103d77470b0b41e1af93
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2023-10-17 21:44:28 -0300

    Update index.php

--- relatorio_internet_v-txt_v2/index.php
+++ relatorio_internet_v-txt_v2/index.php
@@ -18,14 +18,14 @@
     $_valor_status_array_LAN = file_get_contents ($_local_arq_LAN);
     $_status_LAN = explode("-", $_valor_status_array_LAN);
     } else {
-        $_status_WAN = "xx - xx - <desconhecido>";
+        $_status_LAN = "ONLINE";
     }
 
     if (file_exists($_local_arq_WAN)) {
     $_valor_status_array_WAN = file_get_contents($_local_arq_WAN);
     $_status_WAN = explode("-", $_valor_status_array_WAN);
     }  else {
-        $_status_WAN = "xx - xx - <desconhecido>";
+        $_status_WAN = "ONLINE";
     }
 ?>
 <body>
