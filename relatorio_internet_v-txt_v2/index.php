54c67ee1c47fb4252ea13dc25b286c3343c3ffd0
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2023-10-17 21:27:58 -0300

    Update index.php

--- relatorio_internet_v-txt_v2/index.php
+++ relatorio_internet_v-txt_v2/index.php
@@ -8,20 +8,26 @@
 </head>
 
 <?php 
+    // Variaveis de local arquivo
     $_local_arq_LAN = "_status_LAN.log";
     $_local_arq_WAN = "_status_WAN.log";
+
+    // Transformando os dados em array
     $_valor_status_array_LAN = file_get_contents ($_local_arq_LAN);
     $_valor_status_array_WAN = file_get_contents($_local_arq_WAN);
     $_status_LAN = explode("-", $_valor_status_array_LAN);
     $_status_WAN = explode("-", $_valor_status_array_WAN);
 ?>
 <body>
-    <main>
-        <h1>Monitoramento</h1>
-        <div>
-        <?php 
-            print_r("$_status_LAN[0]");
-        ?>
+    <main>        
+        <div class="div_lan">
+        <h1>Status da Rede Local</h1>
+            <?=$_status_LAN[2]?>   
+
+        </div>
+        <h1>Status da internet</h1>
+        <div class="div_wan">
+            <?=$_status_WAN[2]?>
         </div>
     </main>
 </body>
