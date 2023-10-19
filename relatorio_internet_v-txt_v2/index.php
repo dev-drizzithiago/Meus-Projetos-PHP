ad3292054fa9abae4b2de63f5b928a6ae3284ec1
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2023-10-19 16:25:06 -0300

    Update index.php

--- relatorio_internet_v-txt_v2/index.php
+++ relatorio_internet_v-txt_v2/index.php
@@ -9,22 +9,36 @@
 </head>
 
 <?php 
+    // Varias Globais
+    $_contato_lan = 0;
+    $_contato_wan = 0;
+
+    // Variaveis data/hora
+    date_default_timezone_set('America/Sao_Paulo');
+    $_data_atual = date("d/m/Y");
+    $_hora_atual = date("h:m:s");
+
     // Variaveis de local arquivo
     $_local_arq_LAN = "_status_LAN.log";
     $_local_arq_WAN = "_status_WAN.log";
 
-    // Transformando os dados em array
+    // Transformando os dados da lan em array
     if (file_exists($_local_arq_LAN)) {
     $_valor_status_array_LAN = file_get_contents ($_local_arq_LAN);
     $_status_LAN = explode("-", $_valor_status_array_LAN);
-    $_valor_status_LAN = $_status_LAN[2];
+    $_valor_data_LAN = $_status_LAN[0];
+    $_valor_horario_LAN = $_status_LAN[1];
+    $_valor_status_LAN = $_status_LAN[2];    
     } else {
         $_status_LAN[2] = "DESCONHECIDO";
     }
 
+    // Transformando os dados da wan em array
     if (file_exists($_local_arq_WAN)) {
     $_valor_status_array_WAN = file_get_contents($_local_arq_WAN);
     $_status_WAN = explode("-", $_valor_status_array_WAN);
+    $_valor_data_WAN = $_status_WAN[0];
+    $_valor_horario_WAN = $_status_WAN[1];
     $_valor_status_WAN = $_status_WAN[2];
     }  else {
         $_status_WAN[2] = "DESCONHECIDO";
@@ -32,27 +46,32 @@
 ?>
 <body>
     <!--img src="/relatorio_internet_v-txt_v2/img/img_001_ping_v2_off.jpg" alt="OFF"-->
-<main>        
+<main>
+    <div chass="div_data">
+        <?="<h1>$_data_atual</h1>"?>
+        <?="<h1>$_hora_atual</h1>"?>
+    </div>
+
     <div class="div_lan">
     <h1>Status da Rede Local(LAN)</h1>
         <?php
             if ($_valor_status_LAN == " INATIVO") {
-                echo '<img src="img/img_001_ping_v2_off.jpg">';
+                echo '<img src="img/img_003_ping_v2_off.jpg">';
             } else {
-                echo '<img src="img/img_001_ping_v2_on.jpg">';
+                echo '<img src="img/img_003_ping_v2_on.jpg">';
             }
         ?>
     </div>
 
     <div class="div_wan">
-
     <h1>Status da internet(WAN)</h1>
     <?php
             if ($_valor_status_WAN == " INATIVO") {
-                echo '<img src="img/img_001_ping_v2_off.jpg">';
+                echo '<img src="img/img_003_ping_v2_off.jpg">';
             } else {
-                echo '<img src="img/img_001_ping_v2_on.jpg">';
+                echo '<img src="img/img_003_ping_v2_on.jpg">';
             }
+
         ?>
     </div>
 </main>
