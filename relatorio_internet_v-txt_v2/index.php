02f6c47644f817a0812f910cd7f51c9c3cc2651b
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2023-10-20 15:23:04 -0300

    Update index.php

--- relatorio_internet_v-txt_v2/index.php
+++ relatorio_internet_v-txt_v2/index.php
@@ -77,7 +77,11 @@
                     echo '<img src="img/img_003_ping_v2_on.jpg">'; 
                 }
             } elseif ($_valor_status_LAN == " INATIVO") {
-                echo '<img src="img/img_003_ping_v2_off.jpg">';
+                if ($_valor_hora_LAN.$_valor_minu_LAN < $_valor_hora_global.$_valor_minu_global) {
+                    echo "<img src='img/img_003_ping_v2_neutro.jpg'";
+                } else {
+                    echo '<img src="img/img_003_ping_v2_off.jpg">';
+                }
             } if ($_valor_hora_LAN.$_valor_minu_LAN < $_valor_hora_global.$_valor_minu_global) {
                 echo "<img src='img/img_003_ping_v2_neutro.jpg'";
             }
@@ -90,10 +94,13 @@
             if ($_valor_status_WAN == " ATIVO") {
                 if ($_valor_hora_WAN.$_valor_minu_WAN <> $_valor_hora_global.$_valor_minu_global) {
                     echo "<img src='img/img_003_ping_v2_neutro.jpg'>";
-                }   else {
+                } else {
                     echo '<img src="img/img_003_ping_v2_on.jpg">';
                 }
             } elseif ($_status_WAN == " INATIVO") {
+                if ($_valor_hora_WAN.$_valor_minu_WAN <> $_valor_hora_global.$_valor_minu_global)
+                    echo "<img src='img/img_003_ping_v2_neutro.jpg'>";
+            } else {                    
                 echo '<img src="img/img_003_ping_v2_off.jpg">';
             } 
         ?>
